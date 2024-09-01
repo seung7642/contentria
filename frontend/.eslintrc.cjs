cc35b4e3c827f968d7dc5a868c464397df06234c
이승호 <seung7642@gmail.com>
2024-09-01 22:32:31 +0900

    chore: ESLint 설정 추가

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    "next/core-web-vitals",
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  }
+}
