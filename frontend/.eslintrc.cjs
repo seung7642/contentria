71c63ad4b620395a4e3e3a954b598bb799427745
이승호 <seung7642@openbase.co.kr>
2024-09-13 13:33:35 +0900

    chore: ESLint 설정 수정 및 의존성 수정

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,15 +1,8 @@
 module.exports = {
   extends: [
-    "next/core-web-vitals",
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'plugin:prettier/recommended',
-  ],
-  plugins: ['@typescript-eslint', 'prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-  }
+    'next',
+    'next/core-web-vitals',
+    'next/typescript',
+    'prettier'
+  ]
 }
