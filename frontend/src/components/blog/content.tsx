64cd16627cde8f999f6895c94172531589fd429e
이승호 <seung7642@gmail.com>
2025-06-04 09:53:17 +0900

    .

--- frontend/src/components/blog/content.tsx
+++
@@ -1,9 +0,0 @@
-interface ContentProps {
-  children: React.ReactNode;
-}
-
-const Content: React.FC<ContentProps> = ({ children }) => {
-  return <div className="flex-grow p-4">{children}</div>;
-};
-
-export default Content;
