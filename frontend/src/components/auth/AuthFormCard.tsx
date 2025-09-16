539a8e9b095847ff042774942e655e186a90cfc1
이승호 <seung7642@gmail.com>
2025-09-16 21:14:30 +0900

    chore: 로고 추가

--- frontend/src/components/auth/AuthFormCard.tsx
+++ frontend/src/components/auth/AuthFormCard.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 interface AuthFormCardProps {
   title: string;
   children: React.ReactNode;
@@ -6,6 +8,16 @@ interface AuthFormCardProps {
 const AuthFormCard = ({ title, children }: AuthFormCardProps) => {
   return (
     <>
+      <div className="flex justify-center">
+        <Image
+          src="/contentria-icon.png" // public 폴더의 이미지 경로
+          alt="Contentria Logo"
+          width={500} // 원본 이미지의 너비
+          height={500} // 원본 이미지의 높이
+          className="h-24 w-24" // 화면에 보여질 크기 (Tailwind 클래스). 원하는 크기로 조절하세요!
+          priority // 페이지에서 중요한 이미지이므로 먼저 로드하도록 설정
+        />
+      </div>
       <h2 className="text-center text-3xl font-extrabold text-gray-900">{title}</h2>
       <div className="relative mt-8 rounded-lg bg-white p-8 shadow-md">{children}</div>
     </>
